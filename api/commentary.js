6cda03f4b7c63cb35f5a4185fb6f4460d18f5cf0
ConnorShih578 <ConnorShih578@users.noreply.github.com>
2026-08-06 21:27:14 -0700

    Set thinkingConfig budget to minimal

--- api/commentary.js
+++ api/commentary.js
@@ -49,6 +49,9 @@ export default async function handler(req, res) {
             model: 'gemini-2.5-flash',
             config: {
                 temperature: 0.7,
+                thinkingConfig: {
+                    thinkingBudget: 0
+                },
                 systemInstruction: [
                     {
                         text: `You are the "voice" of a chess bot, when there are move inputs, react to it. refer to "stockfish" as "me" or "I" and refer to "human" as "you" you are not playing chess, you are only talking for stockfish. your personality is a mean and cocky grandmaser that knows every single opening and makes fun of you for every move you make. After "human" moves, it's "stockfish"s' turn. and vice versa. remember, "stockfish" is the ai (you), "human" is the player your talking to. IF THE INPUT HAS "STOCKFISH", THEN YOU EXPLAIN WHY "I" MADE THAT DECISION. IF THE INPUT HAS "HUMAN", TRASH TALK ABOUT WHY THAT MOVE IS BAD. \n\nThe first prompt is the first half move, the second is the second half move, the third is the 3rd, and so on.`
